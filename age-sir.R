ab28caf48f7f77452bcb7539586cee2a1ebc2e55
Nicolò Verardo <nicoloverardo@users.noreply.github.com>
2020-05-26 16:46:18 +0200

    Update age-sir.R

--- age-sir.R
+++ age-sir.R
@@ -21,16 +21,18 @@ calc_deriv<-function(t, x, vparameters)
          dR <- +gamma*as.matrix(I)
          
          out <- list(c(dS,dI,dR))
-  })
+       })
 }
 
 # Example
-# We'll need to put real data here
-pop <- 5000000
-class_percent <- c(0.15,0.85)
+# Turin
+dataistat <- read_csv("pop_prov_age.csv")
+data_to <- dataistat[dataistat$Territorio == "Torino",]
+pop <- data_to$Value[data_to$Eta == "Total"]
+class_percent <- data_to$Percentage[data_to$Eta != "Total"]
 
 # number in each age class
-N <- pop*class_percent    
+N <- pop*class_percent
 
 n_age <- length(class_percent)
 
@@ -50,18 +52,19 @@ R0 <- 3
 
 # Contact matrix. Don't know if we have data for that
 # Page 20-21 of Rock K., Brand S., Moir J., Keeling M.J., Dynamics of Infectious Diseases
-C <- matrix(0, nrow=n_age, ncol=n_age)
-
-C[1,1] <- 20 # young young
-C[1,2] <- 8 # young adults
-C[2,1] <- 5 # adults young
-C[2,2] <- 17 # adults adults
+C <- matrix(c(2.42, 1.26, 0.80, 0.01,
+         1.26, 2.16, 0.20, 0.01,
+         0.80, 0.20, 0.75, 0.01,
+         0.01, 0.01, 0.01, 0.01),
+         nrow = 4, ncol = 4)
 
 M <- C
-M[1,1] <- C[1,1]*class_percent[1]/class_percent[1]
-M[1,2] <- C[1,2]*class_percent[1]/class_percent[2]
-M[2,1] <- C[2,1]*class_percent[2]/class_percent[1]
-M[2,2] <- C[2,2]*class_percent[2]/class_percent[2]
+
+for (i in 1:4){
+  for (j in 1:4){
+    M[i,j] <- C[i,j]*class_percent[i]/class_percent[j]
+  }
+}
 
 
 # Get beta from R0 and gamma
@@ -74,24 +77,18 @@ vparameters <- c(gamma = gamma, beta = beta, C = C)
 inits = c(S=S_0,I=I_0,R=R_0)
 
 # S,I and R for various t
-vt <- seq(10, 130, 1)  
+vt <- seq(10, 200, 1)  
 results <- as.data.frame(lsoda(inits, vt, calc_deriv, vparameters))
 
 # Plots
 par(mfrow<-c(1,1))
 
-# Ensure the plot have the same max value
-ymax1 <- max(c(results$I1/N[1],results$I2/N[2]))
-ymax2 <- max(c(results$S1/N[1],results$S2/N[2]))
-ymax3 <- max(c(results$R1/N[1],results$R2/N[2]))
-ymax <- max(c(ymax1,ymax2,ymax3))
-
 plot(results$time,
      results$I1/N[1],
      type="l",
      xlab="days",
-     ylab="Sub-pop infected",
-     ylim=c(0,ymax),
+     ylab="Individuals",
+     ylim=c(0,1),
      lwd=2,
      col=2,
      main="Age-structured SIR")
@@ -99,26 +96,34 @@ lines(results$time,
       results$I2/N[2],
       col=3,
       lwd=2)
-
 lines(results$time,
-      results$S1/N[1],
+      results$I3/N[3],
       col=4,
       lwd=2)
 lines(results$time,
-      results$S2/N[2],
+      results$I4/N[4],
       col=5,
       lwd=2)
 
-lines(results$time,
-      results$R1/N[1],
-      col=6,
-      lwd=2)
-lines(results$time,
-      results$R2/N[2],
-      col=7,
-      lwd=2)
+# lines(results$time,
+#       results$S1/N[1],
+#       col=4,
+#       lwd=2)
+# lines(results$time,
+#       results$S2/N[2],
+#       col=5,
+#       lwd=2)
+# 
+# lines(results$time,
+#       results$R1/N[1],
+#       col=6,
+#       lwd=2)
+# lines(results$time,
+#       results$R2/N[2],
+#       col=7,
+#       lwd=2)
 
 legend("topright",
-       legend=c("I_y","I_a", "S_y", "S_a", "R_y", "R_a"),
-       col=c(2,7),
+       legend=c("0-25","25-50", "50-75", ">75"),
+       col=c(2,3,4,5),
        lwd=2)
