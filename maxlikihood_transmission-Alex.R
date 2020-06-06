1c7425a6ebe041c868a549a05d0a07120b170ef9
Nicolò Verardo <nicoloverardo@users.noreply.github.com>
2020-06-06 15:41:13 +0200

    Update maxlikihood_transmission-Alex.R

--- maxlikihood_transmission-Alex.R
+++ maxlikihood_transmission-Alex.R
@@ -1,18 +1,33 @@
 library(stats)
 library(stats4)
 library(caret)
-highrisk <-  125840
-mediumrisk <- 166444 
-lowrisk <-  831160
-pij <- 1
 
-x <-sample(highrisk,1000)
-d <- sample(mediumrisk,1000)
+# Example
+# Turin
+dataistat <- read.csv("pop_prov_age.csv")
+data_to <- dataistat[dataistat$Territorio == "Torino",]
+pop <- data_to$Value[data_to$Eta == "Total"]
+class_pop <- data_to$Value[data_to$Eta != "Total"]
+
+# We're gonna need to turn this into 4 groups and
+# calculate this for each group
+
+# 75-100
+highrisk <- class_pop[4]
+
+# 25-50 + 50-75
+mediumrisk <- class_pop[2] + class_pop[3]
+
+x <- sample(highrisk, 1000)
+d <- sample(mediumrisk, 1000)
+
 #from google mobility data, community is divided into PARKS,SHOPS,WORK AND RECREATION
 #AND BY MAKING A MARKOV CHAIN,it seems high risk and medium risk people has most interation 
 #where high risk people r morevurnerable to get the disease  so selected p=0.40
-idx <- createDataPartition(x, p=0.40,list=FALSE)
+
+idx <- createDataPartition(x, p=0.40, list=FALSE)
 k <- rnorm(d)
+
 nll <- function(theta0,theta1) {
   x1 <- as.vector(k)
   y <- as.vector(x[-idx])
@@ -20,11 +35,14 @@ nll <- function(theta0,theta1) {
   -sum(y*(log(mu)) - mu)
 }
 
+x1 <- as.vector(k)
+y <- as.vector(x[-idx])
 
-
+# MLE
 est <- stats4::mle(minuslog=nll, start=list(theta0=2,theta1=0))
 summary(est)
-y1<-  sample(x1,600)
+
+y1<- sample(x1,600)
 w <- data.frame(y,y1)
 
 glm.fit <-  glm(log(y)~y1,data=w)
