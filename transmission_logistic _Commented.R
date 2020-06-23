2dce40f2584b413c5c9f5c911ebbb7cb1d0e7c1a
Alex Aman <60818228+Alexamannn@users.noreply.github.com>
2020-06-23 18:12:39 +0200

    updated _

--- transmission_logistic _Commented.R
+++ transmission_logistic _Commented.R
@@ -1,21 +1,22 @@
 library(stats)
 library(stats4)
 library(caret)
-
+library(glm2)
 # Example
 # Turin
-# Load data from Istat csv
-dataistat <- read.csv("data/istat/pop_prov_age_3_groups.csv")
-data_to <- dataistat[dataistat$Territorio == province,]
+dataistat <- read.csv("https://raw.githubusercontent.com/nicoloverardo/epidemic-models/master/data/istat/pop_prov_age_4_groups.csv")
+data_to <- dataistat[dataistat$Territorio == "Torino",]
 pop <- data_to$Value[data_to$Eta == "Total"]
 class_pop <- data_to$Value[data_to$Eta != "Total"]
-  
+
+# We're gonna need to turn this into 4 groups and
+# calculate this for each group
+
 # 75-100
-highrisk <- class_pop[3]
-# 25-75
-mediumrisk <- class_pop[2]
-# 0-25
-lowrisk <- class_pop[1]
+highrisk <- class_pop[4]
+
+# 25-50 + 50-75
+mediumrisk <- class_pop[2] + class_pop[3]
 
 #finding how already infected high risk people can infect medium risk people
 
@@ -25,20 +26,20 @@ d <- sample(highrisk) #getting sample/combination of population
 #p is division between people infected and not infected in a population.
 #p=0.40  means 40 percent of population is not infected
 
-idx <- createDataPartition(x, p=0.40, list=FALSE)
+idx <- createDataPartition(x, p=0.60, list=FALSE)
 
 
 #x1  ,simply x, population medium risk
 #y  , high risk - non infected population = infected high risk population
 
 x1 <- as.vector(x)
-y <- as.vector(d[-idx])
+y2 <- (d[-idx])
+y <- sample(y2)
 
 #j is y,just to make th elenght of data equal with x1(to make same rows and coloumns for w)
-
-max.len = max(length(x1), length(y))
-j = c(y, rep(NA, max.len - length(y)))
-
+lenght = NROW(y)
+#max.len = max(length(y), length(x1))
+k=sample(x1,lenght)
 # w is a dataframe
 # 1 w shows various combinations of infected high risk with population medium risk
 # means how many combinations of infected population can come in contact with
@@ -46,12 +47,12 @@ j = c(y, rep(NA, max.len - length(y)))
 #for example 100 infected people come in contact with 5000 mediun risk people
 #different possible combinations
 
-w <- data.frame(x1,j)
+w <- data.frame(k,y)
 # regressing with probility that how different combinations of infected people
 #can actually transmit disesase to medium risk people.. from w
 
 ######3 i think we need to add a BIAS for every agr gropu to get different ratios
-glm.fit <-  glm((x1)~j,data=w,family = poisson)
-pred.lm <- predict(glm.fit, w)
-plot(pred.lm)
-summary(pred.lm)
+glm.fit <-  glm((k)~y,data=w,family = poisson)
+
+summary(glm.fit)
+glm.fit$coefficients
